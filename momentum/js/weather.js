13711aebd3c2bb38bdda7b812cea7392812a0798
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-18 13:32:43 +0300

    feat: add wether local storeg

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,5 +1,7 @@
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
+const windSpeed = document.querySelector(`.wind`);
+const humidity = document.querySelector(`.humidity`)
 const weatherDescription = document.querySelector('.weather-description');
 const city = document.querySelector('.city');
 
@@ -12,14 +14,24 @@ async function getWeather() {
   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
   temperature.textContent = `${data.main.temp.toFixed(0)} °C`;
   weatherDescription.textContent = data.weather[0].description;
+  windSpeed.textContent = `Скорость ветра ${data.wind.speed.toFixed(0)} м/cек`;
+  humidity.textContent = `Влажность ${data.main.humidity}%`
 }
 
 function setCity(event) {
   if (event.code === 'Enter') {
     getWeather();
     city.blur();
+    localStorage.setItem('city', city.value)
   }
 }
 
 document.addEventListener('DOMContentLoaded', getWeather);
 city.addEventListener('keypress', setCity);
+
+if (localStorage.getItem('city')) {
+  city.value = localStorage.getItem('city');
+    city.oninput = () => {
+      localStorage.setItem('city', city.value)
+  };
+}
